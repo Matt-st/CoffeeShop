1e64b0cb2c7b75bce61b9a65e514976c44191974
Matt-st <matthew.thorne.s@gmail.com>
2019-01-12 20:25:32 -0500

    Add the spring cloud gateway with routes for the query service.

--- cs-query/src/main/java/coffeeshop/app/CoffeeShopQuery.java
+++ cs-query/src/main/java/coffeeshop/app/CoffeeShopQuery.java
@@ -3,14 +3,6 @@ package coffeeshop.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
