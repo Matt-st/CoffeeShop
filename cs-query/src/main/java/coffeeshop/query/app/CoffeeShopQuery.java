376c4f4da6eae43e26581e93f21a38ed1db0abf1
Matt-st <matthew.thorne.s@gmail.com>
2019-01-23 18:24:13 -0500

    remove unused properties files.  Added bootstrap yml files to each project that uses the discovery server.

--- cs-query/src/main/java/coffeeshop/query/app/CoffeeShopQuery.java
+++ cs-query/src/main/java/coffeeshop/query/app/CoffeeShopQuery.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-
 public class CoffeeShopQuery {
 
     public static void main(String[] args) {
