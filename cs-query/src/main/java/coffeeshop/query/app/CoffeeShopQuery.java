c2f8ff3e9e04503f2afba003cb71dbeb38c6c1c0
Matt-st <matthew.thorne.s@gmail.com>
2019-01-26 16:32:37 -0500

    Changed sleuth configuration and got it working.

--- cs-query/src/main/java/coffeeshop/query/app/CoffeeShopQuery.java
+++ cs-query/src/main/java/coffeeshop/query/app/CoffeeShopQuery.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
-@EnableDiscoveryClient
+import brave.sampler.Sampler;
+
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
 public class CoffeeShopQuery {
@@ -13,6 +16,7 @@ public class CoffeeShopQuery {
     public static void main(String[] args) {
         SpringApplication.run(CoffeeShopQuery.class, args);
     }
+    
     
 }
 
